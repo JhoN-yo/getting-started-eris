8c6b8907f585b310bee341a67de0dc1d7452bfe1
Jhon.ss <70863105+Dev-JhoN@users.noreply.github.com>
2022-08-15 18:16:17 -0300

    feat: error/logger tracker

---
+++ source/structures/util/Logger.js
@@ -0,0 +1,38 @@
+const Embed = require('../embed/Embed');
+const embed = new Embed();
+const c = require('colors');
+const moment = require('moment');
+moment.locale('pt-br');
+
+module.exports = class Logger {
+	constructor(client) {
+		this.client = client;
+	}
+	static send(message) {
+		return console.log(c.green(`[${moment(Date.now()).format('hh:mm')}] ${message}`));
+	}
+	static warn(message) {
+		return console.log(c.yellow(`[${moment(Date.now()).format('hh:mm')}] ${message}`));
+	}
+	async error(error) {
+		embed.setTitle(
+			'Ocorreu um erro inesperado...',
+			'https://cdn.discordapp.com/emojis/1005192372738601001.webp?size=128&quality=lossless',
+		);
+		embed.setDescription(`\`\`\`fix\n${error.stack}\`\`\``);
+		const channels = await this.client.getRestGuildChannels('1002270891985604689');
+		for (const channel of channels) {
+			if (channel.id !== '1002270892862214246') continue;
+			const webhooks = await channel.getWebhooks();
+			var webhook = webhooks.filter((w) => w.name === `${this.client.user.username} Tracker`)[0];
+			if (!webhook)
+				webhook = await channel.createWebhook({ name: this.client.user.username, avatar: this.client.user.avatarURL });
+			this.client.executeWebhook(webhook.id, webhook.token, {
+				embed,
+				username: `${this.client.user.username} Tracker`,
+				avatarURL: this.client.user.avatarURL,
+			});
+			console.log(c.red(`[${moment(Date.now()).format('hh:mm')}] ${error.stack}`));
+		}
+	}
+};
